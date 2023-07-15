22d4d57355b989777680239a0e7aa015f6c2d759
tomoki-dell <fg250j@gmail.com>
2023-07-15 12:29:59 +0900

    tmp

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    assetPrefix: urlPrefix,
-    basePath: urlPrefix,
-    trailingSlash: true,
-}
+const nextConfig = {}
 
 module.exports = nextConfig
